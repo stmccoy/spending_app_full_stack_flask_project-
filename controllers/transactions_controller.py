706ab9150ed30c045fbb2d2960b28bb49496521c
crispymccoy145 <samspam19941@gmail.com>
2021-04-26 10:33:50 +0100

    add transaction done, can use template for debt and direct debits

--- controllers/transactions_controller.py
+++ controllers/transactions_controller.py
@@ -2,7 +2,12 @@ from app import app
 from flask import render_template, request, redirect, url_for
 from flask import Blueprint
 from session import session
+from models.transaction import *
 import repositories.user_repository as user_repository
+import repositories.transaction_repository as transaction_repository
+import repositories.merchant_repository as merchant_repository
+
+
 
 transactions_blueprint = Blueprint('transactions', __name__)
 @transactions_blueprint.route('/transactions')
@@ -19,8 +24,21 @@ def set_budget():
         return redirect(url_for('transactions.transactions'))
     return render_template('transactions/set_budget.html')
 
-@transactions_blueprint.route('/add_transaction')
+@transactions_blueprint.route('/add_transaction', methods=['GET', 'POST'])
 def add_transaction():
+    if request.method == "POST":
+        user = user_repository.select(session)
+        value = request.form['value']
+        description = request.form['description']
+        transaction = Transaction(user, value, description)
+        transaction.date = request.form['date']
+        transaction.priority_rating = request.form['priority_rating']
+        merchant_name = request.form['merchant']
+        #if statement for if merchant = [] do add merchant method else do below
+        merchant = merchant_repository.select_by_name(merchant_name)[0]
+        transaction.merchant = merchant
+        transaction_repository.save(transaction)
+        return redirect(url_for('transactions.transactions'))
     return render_template('transactions/add_transaction.html')
 
 @transactions_blueprint.route('/add_direct_debit')
